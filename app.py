d9fc3021c4e1bc90b5023024a02fa085a0fe56cc
beamchristian <beamchristian@yahoo.com>
2022-11-13 10:43:23 -0500

    test

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 import tkinter as tk
-from tkinter import ttk
-
-def greet():
-    print("Hello, World!")
-
 
 root = tk.Tk()
+root.geometry("600x400")
 
-greet_button = ttk.Button(root, text="Greet", command=greet)
-greet_button.pack(side="left", fill="both", expand=True)
+rectangle_1 = tk.Label(root, text="Rectangle 1", bg="green", fg="white")
+rectangle_1.pack(side="left", ipadx=10, ipady=10, fill="both", expand=True)
 
-quit_button = ttk.Button(root, text="Quit", command=root.destroy)
-quit_button.pack(side="left")
+rectangle_2 = tk.Label(root, text="Rectangle 2", bg="red", fg="white")
+rectangle_2.pack(side="top", ipadx=10, ipady=10, fill="both", expand=True)
 
+
+rectangle_3 = tk.Label(root, text="Rectangle 3", bg="black", fg="white")
+rectangle_3.pack(side="left", ipadx=10, ipady=10, fill="both", expand=True)
 root.mainloop()
